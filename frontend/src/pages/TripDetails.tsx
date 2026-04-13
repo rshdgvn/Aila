a7b2cea2a3d6e3485cb32fbeb10e0cfa854ba65b
rshdgvn <rasheedgavinesponga@gmail.com>
2026-04-14 06:29:58 +0800

    fix: import error

--- frontend/src/pages/TripDetails.tsx
+++ frontend/src/pages/TripDetails.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import polyline from "@mapbox/polyline";
 import {
   ArrowLeft,
