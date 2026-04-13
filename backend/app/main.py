bd13bf5790b3f9554bce0a2af0f279cc02c69828
rshdgvn <rasheedgavinesponga@gmail.com>
2026-04-14 06:37:53 +0800

    remove slash on frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://aila-r.vercel.app/", "http://localhost:5173"],
+    allow_origins=["https://aila-r.vercel.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
